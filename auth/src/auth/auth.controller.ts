eb711837f25a0e833d36e53186fdace012c1becb
VladimirChuprazov <woaouh@icloud.com>
2023-07-26 21:22:08 +0300

    feat: add acces token cookie

--- auth/src/auth/auth.controller.ts
+++ auth/src/auth/auth.controller.ts
@@ -1,20 +1,41 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
-import { AuthEntity } from './entities/auth.entity';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  private handleAuth(accessToken: string, res: Response): void {
+    const cookieExpirationTimeMS = this.configService.get<number>(
+      'COOKIE_EXPIRATION_TIME_MS',
+    );
+    res.cookie('access_token', accessToken, {
+      httpOnly: true,
+      secure: true,
+      expires: new Date(Date.now() + cookieExpirationTimeMS),
+    });
+    res.send({ status: 'ok' });
+  }
 
   @Post('register')
-  async register(@Body() registerDto: RegisterDto): Promise<AuthEntity> {
-    return new AuthEntity(await this.authService.register(registerDto));
+  async register(
+    @Body() registerDto: RegisterDto,
+    @Res() res: Response,
+  ): Promise<void> {
+    const { accessToken } = await this.authService.register(registerDto);
+    await this.handleAuth(accessToken, res);
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto): Promise<AuthEntity> {
-    return new AuthEntity(await this.authService.login(loginDto));
+  async login(@Body() loginDto: LoginDto, @Res() res: Response): Promise<void> {
+    const { accessToken } = await this.authService.login(loginDto);
+    await this.handleAuth(accessToken, res);
   }
 }
